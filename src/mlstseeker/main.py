f98b4155ab0f1e0cfb31331b2b324b2c6e6698c7
sam-baird <sam.baird@state.co.us>
2024-01-27 18:20:44 -0700

    Add location and date filtering for taxon reports

--- src/mlstseeker/main.py
+++ src/mlstseeker/main.py
@@ -1,11 +1,19 @@
 """Entrypoint for program."""
 from . import cli
 from . import datasets
+from . import filtering
 
 
 def main():
     options = cli.parse_args()
     reports = datasets.get_taxon_reports(options.organism)
+    if options.collect_start or options.collect_end:
+        start = options.collect_start
+        end = options.collect_end
+        reports = filtering.filter_reports_by_date(reports, start, end)
+    if options.location:
+        location = options.location
+        reports = filtering.filter_reports_by_location(reports, location)
     if options.cmd == "preview":
         print("Running preview")
     else:
