9ae55fc4ed4cd177e14f5ad4d4d0e0aa24157839
sam-baird <sam.baird@state.co.us>
2024-03-09 23:48:38 -0700

    Add arguments to only the appropriate subcommands

--- src/mlstseeker/cli.py
+++ src/mlstseeker/cli.py
@@ -9,59 +9,70 @@ def parse_args():
         description="Find bacterial genomes on NCBI for a given multi-locus sequence type"
     )
 
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        action=argparse.BooleanOptionalAction,
+        help="do not print log messages"
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action=argparse.BooleanOptionalAction,
+        help="print DEBUG-level log messages"
+    )
     parser = parse_subcommands(parser)
     options = parser.parse_args()
     return options
 
 
 def parse_subcommands(parser):
-    """Add arguments for preview and fetch subcommands"""
+    """Add arguments for preview, fetch, and cache subcommands"""
     subparsers = parser.add_subparsers(dest="command")  # TODO: help msg
     subparsers.required = True
     subparsers.add_parser("preview")
     subparsers.add_parser("fetch")
     subparsers.add_parser("cache")
 
-    for _, subparser in subparsers.choices.items():
-        subparser.add_argument(
+    for subcommand in ("preview", "fetch", "cache"):
+        parser.add_argument(
             "-o",
             "--organism", 
             required=True,
             help="organism or NCBI taxonomy ID"
         )
-
-        subparser.add_argument(
+        parser.add_argument(
             "-t",
             "--type",
             help="multi-locus sequence type (MLST)"
         )
-
-        subparser.add_argument(
+        parser.add_argument(
             "-s",
             "--scheme",
             required=True,
             help="PubMLST scheme name"
         )
 
-        subparser.add_argument(
+    for subcommand in ("preview", "fetch"):
+        subparsers.choices[subcommand].add_argument(
             "--collect-start",
             help="earliest collection year"
         )
 
-        subparser.add_argument(
+        subparsers.choices[subcommand].add_argument(
             "--collect-end",
             help="latest collection year"
         )
-
-        subparser.add_argument(
+        
+        subparsers.choices[subcommand].add_argument(
             "--location",
             help="geographic location of where sample was collected"
         )
 
-        subparser.add_argument(
-            "--cached-only",
-            action=argparse.BooleanOptionalAction,
-            help="only report cached MLST results"
-        )
+    subparsers.choices["cache"].add_argument(
+        "--cached-only",
+        action=argparse.BooleanOptionalAction,
+        help="only report cached MLST results"
+    )
 
     return parser
